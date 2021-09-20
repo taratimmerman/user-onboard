4958e8556020719980e6184bff2ebde8fe644e0d
taratimmerman <timmermantara@gmail.com>
2021-09-20 14:43:43 -0600

    create users array

---
+++ src/components/users.js
@@ -0,0 +1,14 @@
+import { v4 as uuid } from "uuid";
+
+export const users = [
+    {
+        id: uuid(),
+        name: "Tim",
+        email: "tim@onlyemployee.com"
+    },
+    {
+        id: uuid(),
+        name: "Tina",
+        email: "tina@timsalterego.com"
+    }
+];
